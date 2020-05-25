53d048ad028be9f5d0c022e842f2cd2e1b57446b
Egor <egodunaev@gmail.com>
2020-05-26 02:32:41 +0300

    working animations for enemies
    now to add small logic to them

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,22 +5,44 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    private Animator enemyAnimator;
     public NavMeshAgent meshAgent;
+    public float attackDistance;
     public Player player;
     public float viewDistance;
     public Collider hearing;
-    public Collider vision;
 
     // Update is called once per frame
     void Update()
     {
+        meshAgent.isStopped = false;
         float distance = Vector3.Distance(transform.position, player.transform.position);
         //Debug.Log("Distance: " + distance);
+        if (distance <= attackDistance)
+        {
+            Attack();
+        }
         if (distance<viewDistance)
         {
-
+            EnemyStateControl("Walk");
             meshAgent.SetDestination(player.transform.position);
             //Debug.Log("Going to: " + player.transform.position);
         }
+    }
+    void Start()
+    {
+        enemyAnimator = GetComponent<Animator>();
+        EnemyStateControl("IdleState");
+        
+    }
+    void EnemyStateControl(string stateName)
+    {
+        enemyAnimator.SetTrigger(stateName);
+    }
+    void Attack()
+    {
+        meshAgent.isStopped = true;
+        EnemyStateControl("Attack");
+        
     }
 }
